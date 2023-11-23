351a0d35c968e34945fe3cd0ca3880dd5a27b19c
Rohith Krishna <roal2027@gmail.com>
2023-11-23 23:45:34 +0530

    multer images

--- controllers/admin/product.js
+++ controllers/admin/product.js
@@ -9,61 +9,61 @@ const cloudinary = require("../../services/cloudinary")
 // ADD_product
 const createProduct = async (req, res) => {
   try {
-      const productData = req.body;
-      const errors = validationResult(req);
-      
-      if (!errors.isEmpty()) {
-          return res.status(400).json({
-              statusCode: 400,
-              message: "Bad input string",
-              errors: errors.array(),
-          });
-      }
+    const productData = req.body;
+    const errors = validationResult(req);
 
-      const product = await Product.findOne({ name: productData.name });
-
-      if (product) {
-          return res.status(401).json({
-              statusCode: 401,
-              message: "Product already exists",
-              data: product,
-          });
-      }
-
-      if (!req.file) {
-          return res.status(400).json({
-              statusCode: 400,
-              message: "No file found",
-          });
-      }
-
-      const upload = await cloudinary.uploader.upload(req.file.path, { resource_type: "image" });
-
-      const newProduct = new Product({
-          name: productData.name,
-          category: productData.category,
-          type: productData.type,
-          images: upload.secure_url,
-          warranty: productData.warranty,
-          price: productData.price,
-          discount: productData.discount,
-          stock: productData.stock,
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        statusCode: 400,
+        message: "Bad input string",
+        errors: errors.array(),
       });
+    }
 
-      await newProduct.save();
+    const product = await Product.findOne({ name: productData.name });
 
-      res.status(201).json({
-          statusCode: 201,
-          message: "Product created successfully",
-          data: newProduct,
+    if (product) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: "Product already exists",
+        data: product,
       });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({
+        statusCode: 400,
+        message: "No file found",
+      });
+    }
+
+    const upload = await cloudinary.uploader.upload(req.file.path, { resource_type: "image" });
+
+    const newProduct = new Product({
+      name: productData.name,
+      category: productData.category,
+      type: productData.type,
+      images: upload.secure_url,
+      warranty: productData.warranty,
+      price: productData.price,
+      discount: productData.discount,
+      stock: productData.stock,
+    });
+
+    await newProduct.save();
+
+    res.status(201).json({
+      statusCode: 201,
+      message: "Product created successfully",
+      data: newProduct,
+    });
 
   } catch (error) {
-      console.error("Server error:", error);
-      res.status(500).json({
-          statusCode: 500,
-          message: "Internal server error",
-      });
+    console.error("Server error:", error);
+    res.status(500).json({
+      statusCode: 500,
+      message: "Internal server error",
+    });
   }
 };
 
@@ -116,76 +116,94 @@ const getProductById = (productId) => {
     });
 };
 
-
+// --------------------------------------------------
 //   UPDATE_PRODUCT
+// const updateProduct = async (req,res) => {
+//   // (productId, updatedProductData)
+//   try {
+//     console.log(req.params.id);
+//     const product = await Product.findByIdAndUpdate(req.params.id);
+//     if (product) {
+//       console.log("product",);
+//       return res.status(200).json({
+//         statusCode: 200,
+//         message: "product found",
+//     });
+//     }
 
-const updateProduct = async (req,res) => {
-  // (productId, updatedProductData)
+//     else{
+//       return res.status(400).json({
+//         statusCode: 400,
+//         message: "Product not found",
+//     });
+//     }
+
+//   } catch (error) {
+//     console.error("Server error:", error);
+//     res.status(500).json({
+//         statusCode: 500,
+//         message: "Internal server error",
+//     });
+//   }
+
+//   const required = ["name", "category", "warranty","price","discount"]
+//   // const validationError = bodyRequiredDataValidator(body, required);
+//   console.log("productId",productId);
+//   console.log("updatedProductData",updatedProductData);
+//   // if (validationError) {
+//   //   return {
+//   //     statusCode: 400,
+//   //     error: validationError,
+//   //   }
+//   // }
+//   return Product.findByIdAndUpdate(
+//     productId,
+//     updatedProductData,
+//     // { new: true }
+//   )
+//     .then((product) => {
+//       if (!product) {
+//         return {
+//           statusCode: 404,
+//           message: "Product not found",
+//         };
+//       }
+//       return {
+//         statusCode: 200,
+//         message: "Product updated successfully",
+//         data: product,
+//       };
+//     })
+//     .catch((error) => {
+//       return {
+//         statusCode: 500,
+//         message: "Internal Server Error",
+//         error: error.message,
+//       };
+//     });
+// };
+// -----------------------------------------------------
+// update_product
+const updateProduct = async (req, res) => {
   try {
-    console.log(req.params.id);
-    const product = await Product.findByIdAndUpdate(req.params.id);
-    if (product) {
-      console.log("product",);
-      return res.status(200).json({
-        statusCode: 200,
-        message: "product found",
-    });
+    const productId = req.params.id
+    const { } = req.body
+    const product = Product.findById(productId)
+    if (!product) {
+      throw new Error("product not found")
     }
-    
-    else{
-      return res.status(400).json({
-        statusCode: 400,
-        message: "Product not found",
-    });
-    }
+    let uploadimg
+    if (req.file) {
 
+      uploadimg = await cloudinary.uploader.upload(req.file.path, { resource_type: "image" });
+      console.log("uploadimg", uploadimg);
+    }
+    // const 
   } catch (error) {
-    console.error("Server error:", error);
-    res.status(500).json({
-        statusCode: 500,
-        message: "Internal server error",
-    });
+    console.error(error);
   }
-
-  const required = ["name", "category", "warranty","price","discount"]
-  // const validationError = bodyRequiredDataValidator(body, required);
-  console.log("productId",productId);
-  console.log("updatedProductData",updatedProductData);
-  // if (validationError) {
-  //   return {
-  //     statusCode: 400,
-  //     error: validationError,
-  //   }
-  // }
-  return Product.findByIdAndUpdate(
-    productId,
-    updatedProductData,
-    // { new: true }
-  )
-    .then((product) => {
-      if (!product) {
-        return {
-          statusCode: 404,
-          message: "Product not found",
-        };
-      }
-      return {
-        statusCode: 200,
-        message: "Product updated successfully",
-        data: product,
-      };
-    })
-    .catch((error) => {
-      return {
-        statusCode: 500,
-        message: "Internal Server Error",
-        error: error.message,
-      };
-    });
-};
-
-
-
+}
+// -----------------------------------------------------
 // DELETE_PRODUCT
 const deleteProduct = (productId) => {
   return Product.findByIdAndDelete(productId)
