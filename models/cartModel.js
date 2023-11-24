1aa145484d6431615c30b8a0da4cc7d30b1b9034
Rohith Krishna <roal2027@gmail.com>
2023-11-24 18:15:08 +0530

    cart model

---
+++ models/cartModel.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema;
+const cartSchema=new Schema({
+    customer:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+    },
+    totalQuantity:{
+        type:Number
+    },
+    totalPrice:{
+        type:Number
+    },
+    totalDiscountprice:{
+        type:Number
+    },
+    products: [
+        {
+          name: {
+            type: mongoose.Types.ObjectId,
+            ref: "Product",
+          },
+          quantity: { type: Number, default: 1, min: 1 },
+          price: Number,
+        },
+      ],
+})
