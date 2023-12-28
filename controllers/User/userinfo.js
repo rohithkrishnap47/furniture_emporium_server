ed26f34a898e7b9bd37a0ada77b489322ed8b99c
Rohith Krishna <roal2027@gmail.com>
2023-12-28 23:57:33 +0530

    controller - shipmentAddress

---
+++ controllers/User/userinfo.js
@@ -0,0 +1,10 @@
+const usermodal = require("../../models/userModel")
+const authModal = require('../../models/authModal');
+
+const shipmentAddress = async (req, res) => {
+    try {
+        const user=await aut
+    } catch (error) {
+        
+    }
+}
