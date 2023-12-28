09ec5002175663fe9b1f246ae6ec28935d545c1c
Rohith Krishna <roal2027@gmail.com>
2023-12-29 01:01:12 +0530

    userinfo-updateShipmentAddress

--- controllers/User/userinfo.js
+++ controllers/User/userinfo.js
@@ -3,9 +3,36 @@ const authModal = require('../../models/authModal');
 
 const addShipmentAddress = async (req, res) => {
     try {
-        const user=await authModal.findOne({userid})
-
+        const { firstname, lastname, address, country, state, pincode } = req.body;
+        if (!firstname || !lastname || !address || !country || !state || !pincode)
+            return res.status(400).json({
+                statusCode: 400,
+                message: "feilds missing"
+            });
+        const newshipmentaddress = new usermodal({
+            firstname,
+            lastname,
+            address,
+            country,
+            state,
+            pincode
+        });
+        await newshipmentaddress.save();
+        authModal.findByIdAndUpdate(req.user._id,{userid:newshipmentaddress._id})
+        return res.status(201).json({ message: "Shipment address addedsuccessfully", statusCode: 201 })
     } catch (error) {
-        
+        console.log(error);
+        console.error(error);
     }
+}
+
+const updateShipmentAddress = async(req,res)=>{
+    const userdetails=await authModal.findOne({userid})
+    if(!userdetails){
+        // return res.
+    }
+}
+module.exports = {
+    addShipmentAddress,
+    updateShipmentAddress 
 }
--- routes/user_route.js
+++ routes/user_route.js
@@ -33,4 +33,6 @@ router.get("/listusers", (req, res) => {
 });
 
 
+
+
 module.exports=router
