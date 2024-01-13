bbf33a8529757d26285b2be6b2494bd8418bb2c3
Rohith Krishna <roal2027@gmail.com>
2024-01-13 11:34:56 +0530

    minor-updates-in-orderSchema

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,5 +1,48 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Order = mongoose.model("Order", cartSchema);
-module.exports = Order
+const orderSchema = new Schema({
+    customerId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Auth",
+    },
+    product: {
+        type: mongoose.Types.ObjectId,
+        ref: "Product"
+    },
+    deliveryAddress: {
+        type: mongoose.Types.ObjectId,
+        ref: "Address"
+    },
+    cartDetails: {
+        type: mongoose.Types.ObjectId,
+        ref: "Cart"
+    },
+    paymentMethod: [{
+        couponUsed: {
+            type: mongoose.Types.ObjectId,
+            ref: "Coupon"
+        },
+        status: {
+            type: String,
+            enum: ["Pending", "Success"],
+        }
+
+    }],
+    orderedDate:{
+        type:Date,
+        default:Date.now()
+    },
+    deliveredDate:{
+        type:Date,
+        default:null
+    },
+    deliveryStatus:{
+        type:String,
+        enum:["Pending", "Processing", "Shipped", "Delivered"],
+        default:"Pending"
+    }
+})
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order  
