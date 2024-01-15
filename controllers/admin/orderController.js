9df20a2134d3fc9ddb2ee5806828bfa00da4b2be
Rohith Krishna <roal2027@gmail.com>
2024-01-15 10:52:22 +0530

    order-controller

---
+++ controllers/admin/orderController.js
@@ -0,0 +1,57 @@
+const Order = require('../../models/orderModel');
+
+// Create a new order
+exports.createOrder = async (req, res) => {
+    try {
+        const { customerId, product, deliveryAddress, cartDetails, paymentMethod } = req.body;
+
+        const newOrder = new Order({
+            customerId,
+            product,
+            deliveryAddress,
+            cartDetails,
+            paymentMethod,
+        });
+
+        const savedOrder = await newOrder.save();
+
+        res.status(201).json(savedOrder);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+// Get all orders
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find().populate('customerId product deliveryAddress cartDetails paymentMethod.couponUsed');
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+// Update order status
+exports.updateOrderStatus = async (req, res) => {
+    try {
+        const { orderId } = req.params;
+        const { deliveryStatus, deliveredDate } = req.body;
+
+        const updatedOrder = await Order.findByIdAndUpdate(
+            orderId,
+            { deliveryStatus, deliveredDate },
+            { new: true }
+        );
+
+        if (!updatedOrder) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
+        res.status(200).json(updatedOrder);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
