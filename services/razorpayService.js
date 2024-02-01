59357cf34d49dcef8ffa3debfc31534465e46198
Rohith Krishna <roal2027@gmail.com>
2024-02-01 12:05:11 +0530

    minor  changes in razorpay service

--- services/razorpayService.js
+++ services/razorpayService.js
@@ -1,7 +1,9 @@
-const razorpayinstance = () => new Razorpay({
-    key_id: 'YOUR_KEY_ID',
-    key_secret: 'YOUR_KEY_SECRET',
+const Razorpay = require("razorpay");
+const { RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET } = process.env;
+
+const razorpayInstance = () => new Razorpay({
+    key_id: RAZORPAY_KEY_ID,
+    key_secret: RAZORPAY_KEY_SECRET
 });
 
-
-module.exports = razorpayinstance;
+module.exports = razorpayInstance;
