366427c110072e4adc428723fc4b6df99aa7b294
Rohith Krishna <roal2027@gmail.com>
2024-02-10 17:21:40 +0530

    generate a random string function added

--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -10,4 +10,14 @@ const generateOTP = () => {
 
     return OTP;
 }
-module.exports={generateOTP};
+// GENERATE A STRING
+function generateRandomString(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        randomString += charset[randomIndex];
+    }
+    return randomString;
+}
+module.exports={generateOTP,generateRandomString};
