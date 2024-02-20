a248321d3090416abd7e2d085d049af3b26656d3
Rohith Krishna <roal2027@gmail.com>
2024-02-20 14:24:00 +0530

    sendLoginConfirmationEmail-added to utils

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,4 +1,4 @@
-const nodemailer=require("nodemailer")
+const nodemailer = require("nodemailer")
 
 
 const transporter = nodemailer.createTransport({
@@ -12,22 +12,46 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-const sentOTP=(email,otp)=> {
-            const mailOptions = {
-                from: 'filesrkp@gmail.com',
-                to: email,
-                subject: 'Reset Password',
-                text: otp,
-                // html: `<a href='http://127.0.0.1:5501/set_password.html?id=${user._id}' class='button'>SET PASSWORD</a>`
+const sentOTP = (email, otp) => {
+    const mailOptions = {
+        from: 'filesrkp@gmail.com',
+        to: email,
+        subject: 'Reset Password',
+        text: `Dear User,\n\n`
+            + `You have requested to reset your password. Please use the following OTP (One-Time Password) to proceed with the password reset process:\n\n`
+            + `OTP: ${otp}\n\n`
 
-
-            };
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.error('Error sending email:', error);
-                } else {
-                    console.log('Email sent:', info.response);
-                }
-            });
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
         }
-module.exports={sentOTP}
+    });
+}
+
+const sendLoginConfirmationEmail = (email) => {
+    const mailOptions = {
+        from: 'filesrkp@gmail.com',
+        to: email,
+        subject: 'Login Confirmation - Welcome to Furniture Emporium',
+        text: `Dear User,\n\n`
+            + `You have been successfully logged into Furniture Emporium.\n\n`
+            + `We're thrilled to have you with us! Feel free to explore our latest collections and enjoy a seamless shopping experience.\n\n`
+            + `Have a great day!\n\n`
+            + `Best regards,\n`
+            + `The Furniture Emporium Team`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+}
+
+
+module.exports = { sentOTP ,sendLoginConfirmationEmail}
