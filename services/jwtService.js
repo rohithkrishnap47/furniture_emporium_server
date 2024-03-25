e56458109e15dd673bb3fe29a85c5f98ba1fa578
Rohith Krishna <roal2027@gmail.com>
2024-03-25 20:24:19 +0530

    refactored

--- services/jwtService.js
+++ services/jwtService.js
@@ -2,30 +2,30 @@ const jwt = require("jsonwebtoken")
 
 
 exports.verifyUserToken = (token, ignoreExpiration = false) => {
-    return new Promise((resolve, reject) => {
-      try {
-        let decoded = jwt.verify(token, process.env.USER_TOKEN_JWT_SECRET, {
-          ignoreExpiration: ignoreExpiration,
-        });
-        return resolve(decoded);
-      } catch (error) {
-        return reject(error);
-      }
-    });
-  };
-
-
-  exports.getAuthTokenFromHeader = async (req) => {
+  return new Promise((resolve, reject) => {
     try {
-      const {
-        headers: { authorization },
-      } = req;
-      if (authorization && authorization.split(" ")[0] === "Bearer") {
-        return authorization.split(" ")[1];
-      } else {
-        throw new Error(401, "Unauthorized", []);
-      }
+      let decoded = jwt.verify(token, process.env.USER_TOKEN_JWT_SECRET, {
+        ignoreExpiration: ignoreExpiration,
+      });
+      return resolve(decoded);
     } catch (error) {
-      return Promise.reject(error);
+      return reject(error);
     }
-  };
+  });
+};
+
+
+exports.getAuthTokenFromHeader = async (req) => {
+  try {
+    const {
+      headers: { authorization },
+    } = req;
+    if (authorization && authorization.split(" ")[0] === "Bearer") {
+      return authorization.split(" ")[1];
+    } else {
+      throw new Error(401, "Unauthorized", []);
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
